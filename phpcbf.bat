913395a9645672205fafe1892709df82a7e5eb8d
André Amorim <andrefcamorim@gmail.com>
2022-11-01 13:59:44 +0000

    Update path with win vars

--- phpcbf.bat
+++ phpcbf.bat
@@ -4,4 +4,4 @@ if "%PHP_PEAR_PHP_BIN%" neq "" (
     set PHPBIN=%PHP_PEAR_PHP_BIN%
 ) else set PHPBIN=php
 
-"%PHPBIN%" "c:\Users\andre\AppData\Roaming\Composer\vendor\bin\phpcbf" %*
+"%PHPBIN%" "%APPDATA%\Composer\vendor\bin\phpcbf" %*
